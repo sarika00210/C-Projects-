c783200a3581f58f080413bc48787aa5fce47cf1
SARIKA SRIVASTAVA <101422310+sarika00210@users.noreply.github.com>
2023-10-03 22:34:55 +0530

    Create Simple_Calculator.cpp

---
+++ Simple_Calculator.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+int main() {
+    double num1, num2;
+    char operation;
+
+    std::cout << "Simple Calculator" << std::endl;
+    std::cout << "Enter first number: ";
+    std::cin >> num1;
+
+    std::cout << "Enter operation (+, -, *, /): ";
+    std::cin >> operation;
+
+    std::cout << "Enter second number: ";
+    std::cin >> num2;
+
+    double result;
+
+    switch (operation) {
+        case '+':
+            result = num1 + num2;
+            std::cout << "Result: " << num1 << " + " << num2 << " = " << result << std::endl;
+            break;
+        case '-':
+            result = num1 - num2;
+            std::cout << "Result: " << num1 << " - " << num2 << " = " << result << std::endl;
+            break;
+        case '*':
+            result = num1 * num2;
+            std::cout << "Result: " << num1 << " * " << num2 << " = " << result << std::endl;
+            break;
+        case '/':
+            if (num2 != 0) {
+                result = num1 / num2;
+                std::cout << "Result: " << num1 << " / " << num2 << " = " << result << std::endl;
+            } else {
+                std::cout << "Error: Division by zero is not allowed." << std::endl;
+            }
+            break;
+        default:
+            std::cout << "Invalid operation. Please enter +, -, *, or /." << std::endl;
+            break;
+    }
+
+    return 0;
+}
